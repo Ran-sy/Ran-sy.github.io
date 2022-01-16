25a0943c4e29781ae42aaf599cfdc90530f153cd
Ranim <55930699+Ran-sy@users.noreply.github.com>
2022-01-16 21:59:14 +0000

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,32 @@
+// user Check function
+function userCheck() {
+  let person = prompt("Please enter the password to confirm that you are not an imposter:", "Password");
+  if (person == "lalaland") {
+    document.getElementById("editPage").href = "edit.html";
+  } else {
+    alert('this password is wrong, please try again!');
+  }
+}
+//import values from storage
+document.getElementById("profileImg").src = localStorage.getItem("profileImg");
+//the slide show 
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) { // Next/previous controls
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) { // Thumbnail image controls
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  if (n > slides.length) {slideIndex = 1} 
+  //if index is bigger than the num of slides "4", go back to the first slide
+  if (n < 1) {slideIndex = slides.length}
+  //if index is smaller than the num of slides"0" , go back to the last slide
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
---
+++ editJavaScript.js
@@ -0,0 +1,47 @@
+//import values from storage
+document.getElementById("profileImg").src = localStorage.getItem("profileImg");
+//leaveCheck
+function leaveCheck(){
+    alert('you are leaveing\n BYE');
+} 
+//edit image
+function editImg(){ 
+    let imgURL = prompt("Enter the picture url", "EX: picure.png");
+    if(imgURL == "" || imgURL == null){
+        alert("enter a valid value");
+    }
+    else{
+        if (confirm("Are you sure\nIf you press ok the old picture will be gone for good!")) {
+            localStorage.setItem("profileImg", imgURL);
+            document.getElementById("profileImg").src = imgURL;
+            alert("picture's link updated successfully to " + imgURL);
+        } else {
+            alert('picture update canceled')
+        }
+    }
+}
+//acheivements
+function updateAchievementList(){
+    
+}
+//the slide show 
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) { // Next/previous controls
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) { // Thumbnail image controls
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  if (n > slides.length) {slideIndex = 1} 
+  //if index is bigger than the num of slides "4", go back to the first slide
+  if (n < 1) {slideIndex = slides.length}
+  //if index is smaller than the num of slides"0" , go back to the last slide
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
